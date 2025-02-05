734daf76c7342b3c3c63455ff5f4387c0934f85b
Gabriel Csollei <gcsollei@hotmail.com>
2025-02-05 07:05:09 +0100

    add originToAnchor function

--- workspaces/utils/src/origin-utils.ts
+++ workspaces/utils/src/origin-utils.ts
@@ -1,4 +1,23 @@
 import { Origin } from "@g43/enums";
+import { SimpleVector } from "@g43/math";
+import type { ReadonlySimpleVector2 } from "@g43/types";
+
+export function originToAnchor(origin: Origin): ReadonlySimpleVector2 {
+    switch (origin) {
+        case Origin.TL:
+            return SimpleVector.ZERO_2;
+        case Origin.CENTER:
+            return SimpleVector.HALF_2;
+        case Origin.BR:
+            return SimpleVector.ONE_2;
+        case Origin.BL:
+            return SimpleVector.UP_2;
+        case Origin.TR:
+            return SimpleVector.RIGHT_2;
+        default:
+            return SimpleVector.create(getOriginMultiplierX(origin), getOriginMultiplierY(origin));
+    }
+}
 
 export function getOriginMultiplierX(origin: Origin): 0 | 0.5 | 1 {
     switch (origin) {
