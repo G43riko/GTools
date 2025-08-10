bfc26d59022da77220fb8c9c84aab7273a277a6e
Gabriel Csollei <gcsollei@hotmail.com>
2025-08-10 09:20:52 +0200

    refactor(utils): optimize calcCrow utils

--- workspaces/utils/src/geo-utils.ts
+++ workspaces/utils/src/geo-utils.ts
@@ -1,8 +1,11 @@
 import { toRadians } from "./math-utils.ts";
 
+const RADIUS = 6371; // km
+
 /**
  * Calculates the great-circle distance between two points on the Earth's surface
  * given their latitude and longitude using the haversine formula.
+ * Uses haversine 
  *
  * @param {number} lat1 Latitude of the first point in decimal degrees.
  * @param {number} lon1 Longitude of the first point in decimal degrees.
@@ -11,15 +14,18 @@ import { toRadians } from "./math-utils.ts";
  * @return {number} The great-circle distance between the two points in kilometers.
  */
 export function calcCrow(lat1: number, lon1: number, lat2: number, lon2: number): number {
-    const R = 6371; // km
+
     const dLat = toRadians(lat2 - lat1);
     const dLon = toRadians(lon2 - lon1);
+
     const lat1r = toRadians(lat1);
     const lat2r = toRadians(lat2);
 
-    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1r) * Math.cos(lat2r);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const sinDLat = Math.sin(dLat * 0.5);
+    const sinDLon = Math.sin(dLon * 0.5);
 
-    return R * c;
+    const a = sinDLat * sinDLat + 
+        sinDLon * sinDLon * Math.cos(lat1r) * Math.cos(lat2r);
+
+    return RADIUS * (2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)));
 }
